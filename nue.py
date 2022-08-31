b7c19ef3fec73f7932f7c672536472dfbf80ec65
dev-geinin <112578382+dev-geinin@users.noreply.github.com>
2022-09-01 03:32:34 +0900

    Create nue.py

---
+++ nue.py
@@ -0,0 +1,13 @@
+Main
+
+if(hage&&dev&&smell==bad){
+  switch(feature)
+     case 〇〇:
+         rikon = true
+         break
+     case △△:
+　　rikon = false
+        break
+     default:
+          rikon = undefined 
+}
